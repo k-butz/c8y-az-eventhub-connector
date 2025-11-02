d3b3338165582db60400e3c782ed9281d69866af
Korbinian Butz <korbinian.butz@cumulocity.com>
2025-11-02 21:03:45 +0100

    rename subscription.subscriber configuration

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -94,7 +94,7 @@ func Run() {
 
 func startPipeline(c8yClient *c8y.Client, azureClient *azeventhubs.ProducerClient) {
 	measurementSubscriptionName := viper.GetString("subscription.name")
-	subscriberName := viper.GetString("subscription.subscriber")
+	subscriberName := viper.GetString("subscription.subscriberName")
 	p := Pipeline{
 		Id:          generateBase64ShortID(),
 		C8YClient:   c8yClient,
